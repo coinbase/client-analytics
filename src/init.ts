e86832a2f3d232db74072165139e50dde0e44151
Cooper Willetts <cooper.willetts@coinbase.com>
2023-10-06 10:28:39 -0400

    pr comments and first test

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,8 @@ import { init as configInit } from './storage/config.ts';
 import { init as storageInit } from './storage/storage.ts';
 import { InputConfig } from './types/config.ts';
 import { setLanguageCode, setDevice } from './storage/identity.ts';
+import { setIdentity } from './storage/identity.ts';
+import { SetIdentity } from './types/identity.ts';
 
 export const init = (config: InputConfig) => {
   const configuration = configInit(config);
@@ -9,4 +11,30 @@ export const init = (config: InputConfig) => {
   setLanguageCode();
 
   setDevice();
+};
+
+/**
+ * Sets the User ID and updates user properties for future events.
+ */
+export const identify = (properties: SetIdentity) => {
+  setIdentity(properties);
+  if (properties.userAgent) {
+    setDevice();
+  }
+};
+
+/**
+ * Turns off logging for the user.
+ * No events will be saved or sent to Analytics Service
+ * while this is enabled.
+ */
+export const optOut = () => {
+  setIdentity({ isOptOut: true });
+};
+
+/**
+ * Turns on logging for the user.
+ */
+export const optIn = () => {
+  setIdentity({ isOptOut: false });
 };
---
+++ src/storage/identity.test.ts
@@ -0,0 +1,352 @@
+/* eslint-disable */
+import { getConfig } from './storage';
+import {
+  device,
+  getIsAuthed,
+  identity,
+  setDevice,
+  setDeviceSize,
+  setIdentity,
+  setLanguageCode,
+  getPlatformValue,
+} from './identity';
+
+import { init as setConfig } from './config';
+import { describe, test, expect, beforeEach, afterAll, SpyInstance } from 'vitest';
+
+const customGlobal: any = global;
+
+const resetState = () => {
+  const config = getConfig();
+  device.browserName = null;
+  device.browserMajor = null;
+  device.osName = null;
+  device.userAgent = null;
+  device.height = null;
+  device.width = null;
+  identity.languageCode = null;
+  identity.userAgent = null;
+  identity.countryCode = null;
+  identity.deviceId = null;
+  identity.device_os = null;
+  (identity.isOptOut = false), (identity.languageCode = null);
+  identity.locale = null;
+  identity.session_lcc_id = null;
+  identity.userAgent = null;
+  identity.userId = null;
+  config.isAlwaysAuthed = false;
+  setConfig({
+    platform: 'web',
+    projectName: 'testing',
+  });
+};
+
+describe('identity', () => {
+  const userAgent =
+    'Mozilla/5.0 (darwin) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/11.12.0';
+
+  beforeEach(() => {
+    // jest.resetAllMocks(); - Do we still need this?
+    // jest.clearAllMocks();
+    resetState();
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['en'],
+      configurable: true,
+    });
+    Object.defineProperty(window.navigator, 'userAgent', { value: userAgent });
+    // @ts-expect-error
+    (global as unknown).matchMedia = () => {
+      return {
+        matches: false,
+      };
+    };
+    Object.defineProperty(window, 'innerHeight', { value: 1000 });
+    Object.defineProperty(window, 'innerWidth', { value: 800 });
+  });
+
+  test('should have the right default values', () => {
+    expect(identity).toEqual({
+      countryCode: null,
+      deviceId: null,
+      device_os: null,
+      isOptOut: false,
+      languageCode: null,
+      locale: null,
+      jwt: null,
+      session_lcc_id: null,
+      userAgent: null,
+      userId: null,
+    });
+  });
+
+  describe('getIsAuthed()', () => {
+    beforeEach(() => {
+      // jest.resetAllMocks();
+      resetState();
+    });
+
+    test('should return true when isAlwaysAuthed is set to true', () => {
+      getConfig().isAlwaysAuthed = true;
+      expect(getIsAuthed()).toBe(true);
+    });
+
+    test('should check if userId is set when isAlwaysAuthed is false', () => {
+      identity.userId = 'userId';
+      expect(getIsAuthed()).toBe(true);
+    });
+  });
+
+  describe('setDevice()', () => {
+    beforeEach(() => {
+      // jest.resetAllMocks();
+      resetState();
+    });
+
+    test('should set device when platform is web', () => {
+      setConfig({
+        platform: 'web',
+        projectName: 'testing',
+      });
+      setDevice();
+      expect(device).toEqual({
+        amplitudeDeviceModel: 'browserOS',
+        amplitudeOSName: 'browserName',
+        amplitudeOSVersion: 'browserMajor',
+        amplitudePlatform: 'Web',
+        browserMajor: 'browserMajor',
+        browserName: 'browserName',
+        osName: 'browserOS',
+        height: 1000,
+        width: 800,
+        userAgent,
+      });
+    });
+
+    test('should set device when platform is mobile_web', () => {
+      setConfig({
+        platform: 'mobile_web',
+        projectName: 'testing',
+      });
+      setDevice();
+      expect(device).toEqual({
+        browserMajor: 'browserMajor',
+        browserName: 'browserName',
+        osName: 'browserOS',
+        height: 1000,
+        width: 800,
+        userAgent,
+      });
+    });
+  });
+
+  describe('setIdentity()', () => {
+    beforeEach(() => {
+      //jest.resetAllMocks();
+      resetState();
+    });
+
+    afterAll(() => {
+      // jest.resetAllMocks();
+      identity.countryCode = null;
+      identity.deviceId = null;
+      identity.device_os = null;
+      identity.isOptOut = false;
+      identity.languageCode = null;
+      identity.locale = null;
+      identity.session_lcc_id = null;
+      identity.userAgent = null;
+      identity.userId = null;
+      setConfig({
+        platform: 'web',
+        projectName: 'testing',
+      });
+    });
+
+    test('should set identity', () => {
+      setIdentity({
+        countryCode: 'US',
+        deviceId: 'deviceId',
+        device_os: 'device_os',
+        isOptOut: true,
+        languageCode: 'en-us',
+        locale: 'it',
+        session_lcc_id: 'sessionLccID',
+        userAgent: 'userAgent',
+        userId: 'userId',
+      });
+
+      expect(identity).toEqual({
+        countryCode: 'US',
+        deviceId: 'deviceId',
+        device_os: 'device_os',
+        isOptOut: true,
+        languageCode: 'en-us',
+        locale: 'it',
+        jwt: 'jwtValue',
+        session_lcc_id: 'sessionLccID',
+        userAgent: 'userAgent',
+        userId: 'userId',
+      });
+    });
+
+    test('should only set userId', () => {
+      setIdentity({
+        userId: 'userId',
+      });
+      expect(identity).toEqual({
+        countryCode: null,
+        deviceId: null,
+        device_os: null,
+        isOptOut: false,
+        languageCode: null,
+        locale: null,
+        jwt: null,
+        session_lcc_id: null,
+        userAgent: null,
+        userId: 'userId',
+      });
+    });
+
+    test('should only set deviceId', () => {
+      setIdentity({
+        deviceId: 'deviceId',
+      });
+      expect(identity).toEqual({
+        countryCode: null,
+        deviceId: 'deviceId',
+        device_os: null,
+        isOptOut: false,
+        languageCode: null,
+        locale: null,
+        jwt: null,
+        session_lcc_id: null,
+        userAgent: null,
+        userId: null,
+      });
+    });
+
+    test('should only set session_lcc_id', () => {
+      setIdentity({
+        session_lcc_id: 'session_lcc_id',
+      });
+      expect(identity).toEqual({
+        countryCode: null,
+        deviceId: null,
+        device_os: null,
+        isOptOut: false,
+        languageCode: null,
+        locale: null,
+        jwt: null,
+        session_lcc_id: 'session_lcc_id',
+        userAgent: null,
+        userId: null,
+      });
+    });
+
+    test('should only set device_os', () => {
+      setIdentity({
+        device_os: 'device_os',
+      });
+      expect(identity).toEqual({
+        countryCode: null,
+        deviceId: null,
+        device_os: 'device_os',
+        isOptOut: false,
+        languageCode: null,
+        locale: null,
+        jwt: null,
+        session_lcc_id: null,
+        userAgent: null,
+        userId: null,
+      });
+    });
+
+    test('should only set use agent', () => {
+      setIdentity({
+        userAgent: 'user-agent',
+      });
+      expect(identity).toEqual({
+        countryCode: null,
+        deviceId: null,
+        device_os: null,
+        isOptOut: false,
+        languageCode: null,
+        locale: null,
+        jwt: null,
+        session_lcc_id: null,
+        userAgent: 'user-agent',
+        userId: null,
+      });
+    });
+  });
+
+  describe('setDeviceSize()', () => {
+    beforeEach(() => {
+      // jest.resetAllMocks();
+      resetState();
+      customGlobal.innerWidth = 500;
+      customGlobal.innerHeight = 900;
+    });
+
+    test('should set device height and width', () => {
+      expect(device.height).toBeNull();
+      setDeviceSize({ height: 1000, width: 800 });
+      expect(device.height).toEqual(1000);
+      expect(device.width).toEqual(800);
+    });
+
+    // test('should set device height and width on resize', () => {
+    //     setConfig({
+    //         platform: 'web',
+    //         projectName: 'testing'
+    //       });
+    //   expect(device.height).toBeNull();
+    //   expect(window.innerHeight).toEqual(900);
+    //   initPageResize();
+
+    //   void act(() => {
+    //     customGlobal.innerWidth = 230;
+    //     customGlobal.innerHeight = 315;
+
+    //     fireEvent(customGlobal, new Event('resize'));
+    //   });
+
+    //   expect(device.height).toEqual(315);
+    // });
+  });
+
+  describe('setLanguageCode()', () => {
+    beforeEach(() => {
+      resetState();
+    });
+
+    test('should set languageCode when is web', () => {
+        setConfig({
+            platform: 'web',
+            projectName: 'testing'
+          });      setLanguageCode();
+      expect(identity.languageCode).toBe('en');
+    });
+  });
+
+  describe('getPlatformValue() mobile_web platform', () => {
+    beforeEach(() => {
+      resetState();
+      jest.resetAllMocks();
+      jest.spyOn(isMobileWeb, 'isMobileWeb').mockImplementation(() => true);
+    });
+
+    test('should set platform to mobile_web when isMobileWeb is true', () => {
+      expect(getPlatformValue()).toBe('mobile_web');
+    });
+
+    test('should set platform to mobile_web', () => {
+        setConfig({
+            platform: 'mobile_web',
+            projectName: 'testing'
+          });      expect(getPlatformValue()).toBe('mobile_web');
+    });
+  });
+});
+
+/* eslint-enable */

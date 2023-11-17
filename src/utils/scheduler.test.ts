d909a73419c6b7022bed8e7613801787fee0bb65
Shedon Natnael <shedon.natnael@coinbase.com>
2023-11-17 15:06:22 -0500

    delete comments and fix format

--- src/utils/scheduler.test.ts
+++ src/utils/scheduler.test.ts
@@ -11,19 +11,23 @@ import { getNetworkLayer } from '../storage/storage';
 // };
 
 describe('Scheduler', () => {
-
   describe('Events Scheduler', () => {
-
     let scheduler: Scheduler<Event>;
     const networkLayer = getNetworkLayer();
-    let callbackSpy = vi.spyOn(networkLayer, 'sendEvents').mockImplementation(() => null);
+    let callbackSpy = vi
+      .spyOn(networkLayer, 'sendEvents')
+      .mockImplementation(() => null);
     beforeEach(() => {
       vi.resetAllMocks();
       scheduler = createScheduler<Event>(networkLayer.sendEvents);
     });
 
     test('should add an event', () => {
-      const event = { action: 'unknown', component: 'unknown', name: 'testEvent'};
+      const event = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent',
+      };
       scheduler.add(event);
       expect(scheduler.length).toBe(1);
       expect(callbackSpy).toHaveBeenCalledTimes(0);
@@ -31,7 +35,11 @@ describe('Scheduler', () => {
     });
 
     test('should add/consume an event and access the networkLayer when importance is high', () => {
-      const event = { action: 'unknown', component: 'unknown', name: 'testEvent'};
+      const event = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent',
+      };
       scheduler.add(event, 'high');
       expect(scheduler.length).toBe(0);
       expect(callbackSpy).toHaveBeenCalledTimes(1);
@@ -39,8 +47,16 @@ describe('Scheduler', () => {
     });
 
     test('should add multiple events', () => {
-      const event1 = { action: 'unknown', component: 'unknown', name: 'testEvent1' };
-      const event2 = { action: 'unknown', component: 'unknown', name: 'testEvent2' };
+      const event1 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent1',
+      };
+      const event2 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent2',
+      };
       scheduler.add(event1);
       scheduler.add(event2);
       expect(scheduler.length).toBe(2);
@@ -49,8 +65,16 @@ describe('Scheduler', () => {
     });
 
     test('should add/consume multiple events and access the networkLayer when importance is high', () => {
-      const event1 = { action: 'unknown', component: 'unknown', name: 'testEvent1' };
-      const event2 = { action: 'unknown', component: 'unknown', name: 'testEvent2' };
+      const event1 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent1',
+      };
+      const event2 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent2',
+      };
       scheduler.add(event1, 'high');
       scheduler.add(event2, 'high');
       expect(scheduler.length).toBe(0);
@@ -60,9 +84,21 @@ describe('Scheduler', () => {
 
     test('should consume events when threshold is reached', () => {
       scheduler = createScheduler<Event>(networkLayer.sendEvents, 3);
-      const event1 = { action: 'unknown', component: 'unknown', name: 'testEvent1' };
-      const event2 = { action: 'unknown', component: 'unknown', name: 'testEvent2' };
-      const event3 = { action: 'unknown', component: 'unknown', name: 'testEvent3' };
+      const event1 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent1',
+      };
+      const event2 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent2',
+      };
+      const event3 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent3',
+      };
       scheduler.add(event1);
       scheduler.add(event2);
       scheduler.add(event3);
@@ -74,9 +110,21 @@ describe('Scheduler', () => {
     test('should consume events when time threshold is reached', () => {
       vi.useFakeTimers();
       scheduler = createScheduler<Event>(networkLayer.sendEvents, 5, 100);
-      const event1 = { action: 'unknown', component: 'unknown', name: 'testEvent1' };
-      const event2 = { action: 'unknown', component: 'unknown', name: 'testEvent2' };
-      const event3 = { action: 'unknown', component: 'unknown', name: 'testEvent3' };
+      const event1 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent1',
+      };
+      const event2 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent2',
+      };
+      const event3 = {
+        action: 'unknown',
+        component: 'unknown',
+        name: 'testEvent3',
+      };
       scheduler.add(event1);
       scheduler.add(event2);
       scheduler.add(event3);
@@ -85,21 +133,25 @@ describe('Scheduler', () => {
       expect(scheduler.items).toEqual([]);
       expect(callbackSpy).toHaveBeenCalledTimes(1);
     });
-
-  })
+  });
 
   describe('Metrics Scheduler', () => {
-
     let scheduler: Scheduler<Metric>;
     const networkLayer = getNetworkLayer();
-    let callbackSpy = vi.spyOn(networkLayer, 'sendMetrics').mockImplementation(() => null);
+    let callbackSpy = vi
+      .spyOn(networkLayer, 'sendMetrics')
+      .mockImplementation(() => null);
     beforeEach(() => {
       vi.resetAllMocks();
       scheduler = createScheduler<Metric>(networkLayer.sendMetrics);
     });
 
     test('should add an Metric', () => {
-      const metric = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
+      const metric = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric);
       expect(scheduler.length).toBe(1);
       expect(callbackSpy).toHaveBeenCalledTimes(0);
@@ -107,7 +159,11 @@ describe('Scheduler', () => {
     });
 
     test('should add/consume an Metric and access the networkLayer when importance is high', () => {
-      const metric = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
+      const metric = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric, 'high');
       expect(scheduler.length).toBe(0);
       expect(callbackSpy).toHaveBeenCalledTimes(1);
@@ -115,8 +171,16 @@ describe('Scheduler', () => {
     });
 
     test('should add multiple Metrics', () => {
-      const metric1 = { metricName: 'testMetric1', metricType: MetricType.count, value: 1 };
-      const metric2 = { metricName: 'testMetric2', metricType: MetricType.count, value: 1 };
+      const metric1 = {
+        metricName: 'testMetric1',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric2 = {
+        metricName: 'testMetric2',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric1);
       scheduler.add(metric2);
       expect(scheduler.length).toBe(2);
@@ -125,8 +189,16 @@ describe('Scheduler', () => {
     });
 
     test('should add/consume multiple Metrics and access the networkLayer when importance is high', () => {
-      const metric1 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
-      const metric2 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
+      const metric1 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric2 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric1, 'high');
       scheduler.add(metric2, 'high');
       expect(scheduler.length).toBe(0);
@@ -136,9 +208,21 @@ describe('Scheduler', () => {
 
     test('should consume Metrics when threshold is reached', () => {
       scheduler = createScheduler<Metric>(networkLayer.sendMetrics, 3);
-      const metric1 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
-      const metric2 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
-      const metric3 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
+      const metric1 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric2 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric3 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric1);
       scheduler.add(metric2);
       scheduler.add(metric3);
@@ -150,9 +234,21 @@ describe('Scheduler', () => {
     test('should consume Metrics when time threshold is reached', () => {
       vi.useFakeTimers();
       scheduler = createScheduler<Metric>(networkLayer.sendMetrics, 5, 100);
-      const metric1 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
-      const metric2 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
-      const metric3 = { metricName: 'testMetric', metricType: MetricType.count, value: 1 };
+      const metric1 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric2 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
+      const metric3 = {
+        metricName: 'testMetric',
+        metricType: MetricType.count,
+        value: 1,
+      };
       scheduler.add(metric1);
       scheduler.add(metric2);
       scheduler.add(metric3);
@@ -161,5 +257,5 @@ describe('Scheduler', () => {
       expect(scheduler.items).toEqual([]);
       expect(callbackSpy).toHaveBeenCalledTimes(1);
     });
-  })
+  });
 });
--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -12,13 +12,11 @@ export const DEFAULT_SCHEDULER = {
   length: 0,
 };
 
-export const createScheduler = <T>(sendData: (items: T[]) => void, batchThreshold = DEFAULT_BATCH_THRESHOLD, timeThreshold = DEFAULT_TIME_THRESHOLD): Scheduler<T> => {
-  // option 1: we can create a network layer here
-  // const networkLayer = getNetworkLayer();
-  // need to add a check if metric or event?
-  // networkLayer.sendEvents
-  // networkLayer.sendMetrics
-
+export const createScheduler = <T>(
+  sendData: (items: T[]) => void,
+  batchThreshold = DEFAULT_BATCH_THRESHOLD,
+  timeThreshold = DEFAULT_TIME_THRESHOLD
+): Scheduler<T> => {
   const queue = createQueue<T>();
   const add = (item: T, importance = 'low'): void => {
     queue.add(item);
@@ -32,22 +30,18 @@ export const createScheduler = <T>(sendData: (items: T[]) => void, batchThreshol
     if (queue.length === 0) {
       return;
     }
-    // otherwise we pass all items to the network layer
     sendData(queue.items);
-    //  maybe we can return in the flush method
-    // callback(queue.flush());
     queue.flush();
   };
 
   /**
    * Schedule the consume function to run every timeThreshold
    */
-  const schedule = () => setTimeout(() => {
-    // consume all the items in the queue
-    consume();
-    // reschedule the consume function
-    schedule();
-  }, timeThreshold);
+  const schedule = () =>
+    setTimeout(() => {
+      consume();
+      schedule();
+    }, timeThreshold);
 
   schedule();
 
